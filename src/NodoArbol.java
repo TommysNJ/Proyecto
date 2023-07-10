b59f9834a794d0cfcf20c521471ea9334972f2e2
TommysNJ <TommysNJ@users.noreply.github.com>
2023-07-09 20:05:14 -0500

    Inicio Arbol de decisiones

---
+++ src/NodoArbol.java
@@ -0,0 +1,20 @@
+public class NodoArbol {
+    private
+    private int valor;
+    private NodoArbol nodoIzquierdo;
+    private NodoArbol nodoDerecho;
+
+    public NodoArbol (int valor){
+        this.valor = valor;
+        this.nodoIzquierdo = null;
+        this.nodoDerecho = null;
+    }
+
+    public NodoArbol getNodoIzquierdo() {
+        return nodoIzquierdo;
+    }
+
+    public NodoArbol getNodoDerecho() {
+        return nodoDerecho;
+    }
+}
